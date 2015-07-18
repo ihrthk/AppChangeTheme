6240572ca0574640a7856d9a8cb907db43981a21
ihrthk <ihrthk@gmail.com>
2015-07-18 17:10:35 +0800

    add key code

--- app/src/main/java/me/zhangls/appchangetheme/MainActivity.java
+++ app/src/main/java/me/zhangls/appchangetheme/MainActivity.java
@@ -1,9 +1,14 @@
 package me.zhangls.appchangetheme;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,6 +16,31 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public Resources getAPKResources(Context context, String apkPath) throws Exception {
+        String PATH_AssetManager = "android.content.res.AssetManager";
+        Class assetMagCls = Class.forName(PATH_AssetManager);
+        Constructor assetMagCt = assetMagCls.getConstructor((Class[]) null);
+        Object assetMag = assetMagCt.newInstance((Object[]) null);
+        Class[] typeArgs = new Class[1];
+        typeArgs[0] = String.class;
+        Method assetMag_addAssetPathMtd = assetMagCls.getDeclaredMethod("addAssetPath", typeArgs);
+        Object[] valueArgs = new Object[1];
+//        valueArgs[0] = StorageUtils.getSkinDirectory(mContext) + apkPath.hashCode() + ".theme";
+        assetMag_addAssetPathMtd.invoke(assetMag, valueArgs);
+        Resources res = context.getResources();
+        typeArgs = new Class[3];
+        typeArgs[0] = assetMag.getClass();
+        typeArgs[1] = res.getDisplayMetrics().getClass();
+        typeArgs[2] = res.getConfiguration().getClass();
+        Constructor resCt = Resources.class.getConstructor(typeArgs);
+        valueArgs = new Object[3];
+        valueArgs[0] = assetMag;
+        valueArgs[1] = res.getDisplayMetrics();
+        valueArgs[2] = res.getConfiguration();
+        res = (Resources) resCt.newInstance(valueArgs);
+        return res;
     }
 
     @Override
